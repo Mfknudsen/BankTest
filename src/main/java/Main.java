180f178508a5907f337c93503cff58eaab7ab7ef
LukasCph <71069041+LukasCph@users.noreply.github.com>
2021-02-18 12:05:04 +0100

    added console menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLException;
 import java.util.List;
 import  java.util.Scanner;
 
@@ -7,10 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
+
         try {
-            System.out.println("Type In Username:");
+            System.out.println("Type in your MySQL Username:");
             String user = scanner.nextLine();
-            System.out.println("Type In Password:");
+            System.out.println("Type in your MySQL Password:");
             String password = scanner.nextLine();
             connection = new SQL_Connection(
                     "jdbc:mysql://localhost/?serverTimezone=UTC",
@@ -28,6 +30,41 @@ public class Main {
         }
         catch (Exception e){
             e.printStackTrace();
+        }
+
+
+        boolean finish = false;
+
+        System.out.println("Welcome to the bank, what would you like to do?");
+        System.out.println("Press 1, to deposit money");
+        System.out.println("Press 2, to withdraw money");
+        System.out.println("Press 3, to transfer money to another account");
+        System.out.println("Press 9, to quit the program");
+
+        while (!finish) {
+
+
+            switch (scanner.nextLine()) {
+                case "1":
+                    System.out.println("Enter the name of the account owner and the amount you would like to deposit");
+                    break;
+
+                case "2":
+                    System.out.println("Enter the name of the account owner and the amount you would like to withdraw");
+
+                    break;
+                case "3":
+                    System.out.println("First enter the name of the account owner and the amount you would like to withdraw");
+                    System.out.println("Secondly enter the name of the account owner you would like that amount transfered to");
+                    break;
+
+                case "9":
+                    System.out.println("-------------------------------------------");
+                    System.out.println("Closing down the system");
+                    finish=true;
+                    break;
+            }
+
         }
     }
 }
